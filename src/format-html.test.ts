cc1295c39564d7f9a98f9324d74e5f925b9ea20f
ocavue <ocavue@gmail.com>
2026-01-29 17:30:14 +1100

    feat: fork diffable-html

---
+++ src/format-html.test.ts
@@ -0,0 +1,287 @@
+import { expect, test } from 'vitest'
+
+import format from './format-html'
+
+test('should properly nest everything', () => {
+  const html = `<ul><li><a href="#">List item 1</a></li><li><a href="#">List item 2</a></li></ul>`
+
+  expect(format(html)).toEqual(
+    `
+<ul>
+  <li>
+    <a href="#">
+      List item 1
+    </a>
+  </li>
+  <li>
+    <a href="#">
+      List item 2
+    </a>
+  </li>
+</ul>
+`,
+  )
+})
+
+test('should align attributes vertically', () => {
+  const html = `<input name="test" value="true" class="form-control">`
+
+  expect(format(html)).toEqual(
+    `
+<input
+  name="test"
+  value="true"
+  class="form-control"
+>
+`,
+  )
+})
+
+test('should close tag on the same line if there is only one attribute', () => {
+  const html = `<input  name="test" >`
+
+  expect(format(html)).toEqual(
+    `
+<input name="test">
+`,
+  )
+})
+
+test('should not decode entities', () => {
+  const html = `<div>&nbsp;</div>`
+
+  expect(format(html)).toEqual(
+    `
+<div>
+  &nbsp;
+</div>
+`,
+  )
+})
+
+test('should trim text nodes', () => {
+  const html = `<span> surrounded    </span>`
+
+  expect(format(html)).toEqual(
+    `
+<span>
+  surrounded
+</span>
+`,
+  )
+})
+
+test('should not trim Unicode whitespace', () => {
+  const html = `<span> \u2009 surrounded  \u2005\u200A  </span>`
+
+  expect(format(html)).toEqual(
+    `
+<span>
+  \u2009 surrounded  \u2005\u200A
+</span>
+`,
+  )
+})
+
+test('should not introduce line break if text node is empty', () => {
+  const html = `<span>     </span>`
+
+  expect(format(html)).toEqual(
+    `
+<span>
+</span>
+`,
+  )
+})
+
+test('should not lower case tags', () => {
+  const html = `<Span></Span>`
+
+  expect(format(html)).toEqual(
+    `
+<Span>
+</Span>
+`,
+  )
+})
+
+test('should support xml', () => {
+  const xml = `<?xml version="1.0" encoding="utf-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><url><loc>https://www.example.com</loc></url><url><loc>https://www.example.com/test</loc></url></urlset>`
+
+  expect(format(xml)).toEqual(`
+<?xml
+  version="1.0"
+  encoding="utf-8"
+?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>
+      https://www.example.com
+    </loc>
+  </url>
+  <url>
+    <loc>
+      https://www.example.com/test
+    </loc>
+  </url>
+</urlset>
+`)
+})
+
+test('should keep self-closing tags as they are', () => {
+  const xml = `<xhtml:link href="https://en.example.com" /><xhtml:link href="https://en.example.com" />`
+
+  // in the future this should be a self closing tag instead
+  // see htmlparser2#69
+  expect(format(xml)).toEqual(`
+<xhtml:link href="https://en.example.com">
+</xhtml:link>
+<xhtml:link href="https://en.example.com">
+</xhtml:link>
+`)
+})
+
+test('should support doctype directives', () => {
+  const html = `<!doctype html ><html></html>`
+  expect(format(html)).toEqual(`
+<!doctype html>
+<html>
+</html>
+`)
+})
+
+test('should support html directives', () => {
+  const html = `<!doctype html>
+<html class="no-js" lang="">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>test</title>
+  </head>
+  <body>
+    <!--[if lte IE 9]>
+      <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+    <![endif]-->
+
+    <!-- Add your site or application content here -->
+    <p>Hello world! This is HTML5 Boilerplate.</p>
+    <script src="https://code.jquery.com/jquery-{{JQUERY_VERSION}}.min.js" integrity="{{JQUERY_SRI_HASH}}" crossorigin="anonymous"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+
+    <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+    <script>
+      window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+      ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
+    </script>
+    <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+  </body>
+</html>`
+
+  expect(format(html)).toEqual(`
+<!doctype html>
+<html
+  class="no-js"
+  lang
+>
+  <head>
+    <meta charset="utf-8">
+    <meta
+      http-equiv="x-ua-compatible"
+      content="ie=edge"
+    >
+    <title>
+      test
+    </title>
+  </head>
+  <body>
+    <!--[if lte IE 9]>
+      <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+    <![endif]-->
+    <p>
+      Hello world! This is HTML5 Boilerplate.
+    </p>
+    <script
+      src="https://code.jquery.com/jquery-{{JQUERY_VERSION}}.min.js"
+      integrity="{{JQUERY_SRI_HASH}}"
+      crossorigin="anonymous"
+    >
+    </script>
+    <script src="js/plugins.js">
+    </script>
+    <script src="js/main.js">
+    </script>
+    <script>
+      window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+      ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
+    </script>
+    <script
+      src="https://www.google-analytics.com/analytics.js"
+      async
+      defer
+    >
+    </script>
+  </body>
+</html>
+`)
+})
+
+test('should strip out comments', () => {
+  const html = `<p>Start</p><!-- This comment should be removed --><p>End</p>`
+
+  expect(format(html)).toEqual(
+    `
+<p>
+  Start
+</p>
+<p>
+  End
+</p>
+`,
+  )
+})
+
+test('should strip out empty comments', () => {
+  const html = `<p>Start</p><!----><p>End</p>`
+
+  expect(format(html)).toEqual(
+    `
+<p>
+  Start
+</p>
+<p>
+  End
+</p>
+`,
+  )
+})
+
+test('should not strip out conditional comments', () => {
+  const html = `<p>Start</p><!--[if IE 9]>.... some HTML here ....<![endif]--><p>End</p>`
+
+  expect(format(html)).toEqual(
+    `
+<p>
+  Start
+</p>
+<!--[if IE 9]>.... some HTML here ....<![endif]-->
+<p>
+  End
+</p>
+`,
+  )
+})
+
+test('should sort attributes with `sortAttributes` function', () => {
+  const html = `<input name="test" value="true" class="form-control">`
+  const sortAttributes = (names) => names.sort()
+  expect(format(html, { sortAttributes })).toEqual(
+    `
+<input
+  class="form-control"
+  name="test"
+  value="true"
+>
+`,
+  )
+})
---
+++ src/format-html.ts
@@ -0,0 +1,244 @@
+import { Parser } from 'htmlparser2'
+
+// https://www.w3.org/TR/html/syntax.html#writing-html-documents-elements
+const voidElements = [
+  'area',
+  'base',
+  'br',
+  'col',
+  'embed',
+  'hr',
+  'img',
+  'input',
+  'keygen',
+  'link',
+  'menuitem',
+  'meta',
+  'param',
+  'source',
+  'track',
+  'wbr',
+]
+
+function formatHTML(html, { sortAttributes = (names) => names } = {}) {
+  const elements = []
+  const indentSize = 2
+
+  let currentDepth = 0
+
+  const increaseCurrentDepth = () => {
+    currentDepth++
+  }
+
+  const decreaseCurrentDepth = () => {
+    currentDepth--
+  }
+
+  const getIndentation = (size) => {
+    return ' '.repeat(size)
+  }
+
+  const getIndentationForDepth = (depth) => {
+    return getIndentation(indentSize * depth)
+  }
+
+  const getCurrentIndentation = () => {
+    return getIndentationForDepth(currentDepth)
+  }
+
+  const getAttributeIndentation = (tagName) => {
+    return getIndentation(indentSize * currentDepth - 1)
+  }
+
+  const append = (content) => {
+    elements.push(content)
+  }
+
+  const appendLineBreak = () => {
+    append('\n')
+  }
+
+  const appendIndentation = (depth) => {
+    append(getIndentationForDepth(depth))
+  }
+
+  const appendCurrentIndentation = () => {
+    append(getCurrentIndentation())
+  }
+
+  const appendOpeningTag = (name) => {
+    append('<' + name)
+  }
+
+  const appendClosingTagOnSameLine = (closeWith = '>') => {
+    append(closeWith)
+  }
+
+  const appendClosingTagOnNewLine = (closeWith = '>') => {
+    appendLineBreak()
+    appendIndentation(currentDepth - 1)
+    append(closeWith)
+  }
+
+  const appendAttribute = (name, value) => {
+    let attribute = ' ' + name
+
+    if (value.length > 0) {
+      attribute += `="${value}"`
+    }
+
+    append(attribute)
+  }
+
+  const appendAttributeOnNewLine = (name, value, tagName) => {
+    appendLineBreak()
+    append(getAttributeIndentation(tagName))
+    appendAttribute(name, value)
+  }
+
+  const appendAttributes = (attributes, tagName) => {
+    const names = sortAttributes(Object.keys(attributes))
+
+    if (names.length === 1) {
+      appendAttribute(names[0], attributes[names[0]])
+    }
+
+    if (names.length <= 1) {
+      return
+    }
+
+    for (let name of names) {
+      appendAttributeOnNewLine(name, attributes[name], tagName)
+    }
+  }
+
+  const appendClosingTag = (attributes, closeWith) => {
+    if (Object.keys(attributes).length <= 1) {
+      appendClosingTagOnSameLine(closeWith)
+
+      return
+    }
+    appendClosingTagOnNewLine(closeWith)
+  }
+
+  const render = () => {
+    return elements.join('')
+  }
+
+  const isXmlDirective = (name) => {
+    return name === '?xml'
+  }
+
+  const isVoidTagName = (name) => {
+    return voidElements.includes(name)
+  }
+
+  // https://www.w3.org/TR/html52/infrastructure.html#space-characters
+  // defines "space characters" to include SPACE, TAB, LF, FF, and CR.
+  const trimText = (text) => {
+    return text.replace(/^[\t\n\f\r ]+|[\t\n\f\r ]+$/g, '')
+  }
+
+  const extractAttributesFromString = (content: string) => {
+    const attributes = {}
+
+    const pieces = content.split(/\s/)
+    // Remove tag name.
+    delete pieces[0]
+
+    pieces.forEach((element) => {
+      if (element.length === 0) {
+        return
+      }
+      if (!element.includes('=')) {
+        attributes[element] = ''
+      }
+    })
+
+    const attributesRegex = /(\S+)=["']?((?:.(?!["']?\s+\S+=|["'>]))+.)["']?/gim
+
+    let result
+    while ((result = attributesRegex.exec(content))) {
+      attributes[result[1]] = result[2]
+    }
+
+    return attributes
+  }
+
+  const parser = new Parser(
+    {
+      onprocessinginstruction: function (name, data) {
+        let closingTag = '>'
+        if (isXmlDirective(name)) {
+          closingTag = '?>'
+        }
+
+        appendLineBreak()
+        appendCurrentIndentation()
+        increaseCurrentDepth()
+        appendOpeningTag(name)
+
+        const attributes = extractAttributesFromString(data)
+        appendAttributes(attributes, name)
+        appendClosingTag(attributes, closingTag)
+        decreaseCurrentDepth()
+      },
+      onopentag: function (name, attributes) {
+        appendLineBreak()
+        appendCurrentIndentation()
+        increaseCurrentDepth()
+        appendOpeningTag(name)
+
+        appendAttributes(attributes, name)
+        appendClosingTag(attributes, '>')
+      },
+      ontext: function (text) {
+        const trimmed = trimText(text)
+        if (trimmed.length === 0) {
+          return
+        }
+
+        appendLineBreak()
+        appendCurrentIndentation()
+        append(trimmed)
+      },
+      onclosetag: function (tagname) {
+        const isVoidTag = isVoidTagName(tagname)
+        if (isVoidTagName(tagname) === false) {
+          appendLineBreak()
+        }
+        decreaseCurrentDepth()
+        if (isVoidTag === true) {
+          return
+        }
+        appendCurrentIndentation()
+        append(`</${tagname}>`)
+      },
+      oncomment: function (data) {
+        // Only display conditional comments.
+        if (!data.startsWith('[')) {
+          return
+        }
+        appendLineBreak()
+        appendCurrentIndentation()
+        append('<!--')
+        append(data)
+        append('-->')
+      },
+    },
+    {
+      lowerCaseTags: false,
+      recognizeSelfClosing: true,
+      decodeEntities: false,
+    },
+  )
+  parser.write(html)
+  parser.end()
+
+  appendLineBreak()
+
+  return render()
+}
+
+export { formatHTML }
+export default formatHTML
